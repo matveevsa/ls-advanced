c83d881341f425142b945f4e6317ad7e97b900a7
Matveev Sergey <matveevsa987@gmail.com>
2019-07-05 14:15:55 +0300

    Закончил адаптив

---
+++ src/scripts/popup-menu.js
@@ -0,0 +1,26 @@
+const toggleOpen = document.querySelector('.header .toggle');
+const toggleClose = document.querySelector('.popup-menu__toggle');
+const popupMenu = document.querySelector('.popup-menu');
+const menuLink = popupMenu.querySelectorAll('.socials__link');
+
+toggleOpen.addEventListener('click', (e) => {
+  e.preventDefault();
+  popupMenu.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+});
+
+popupMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  let target = e.target;
+
+  if (target == popupMenu
+      || target == toggleClose
+      || target.className == 'main-nav__link'
+      || target.tagName == 'use'
+      || target.tagName == 'svg') {
+    popupMenu.style.display = 'none';
+    document.body.style.overflow = '';
+  };
+});
+
+
